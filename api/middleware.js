565057ae83405701250575d57eb14a98eade7f48
navishgitlab <navish@mobilyte.com>
2018-07-04 19:48:42 +0530

    authtoken with bearer string setup

--- api/middleware.js
+++ api/middleware.js
@@ -1,16 +1,18 @@
 const jwtToken = require('../lib/auth');
 
-exports.verifyJWT_MW = function(req, res, next)
-{
-  let token = req.headers['x-request-id'];
-  jwtToken.verifyJWTToken(token)
-    .then((decodedToken) =>
-    {
-      req.user = decodedToken;
-      next();
-    })
-    .catch((err) =>
-    {
-      res.send({message: "Invalid auth token provided."})
-    })
+exports.verifyJWT_MW = function (req, res, next) {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    let token = req.headers.authorization.split(' ')[1];
+    jwtToken.verifyJWTToken(token)
+      .then((decodedToken) => {
+        req.user = decodedToken;
+        next();
+      })
+      .catch((err) => {
+        res.send({ message: "Invalid auth token provided." });
+      })
+  } else {
+    res.send({ message: "Auth token type missing." });
+  }
+
 }
